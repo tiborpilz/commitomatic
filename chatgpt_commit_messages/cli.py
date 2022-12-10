ca743aa5a223d4cc32052d168fa4ba02b884fde1
Tibor Pilz <tibor@pilz.berlin>
2022-12-10 13:16:36 +0100

    :wrench: refactor(cli): use rich and typer for cli
    - Move from Click to Typer for a better command line interface
    - Use the rich library for better formatting
    - Use pyperclip to copy the commit message to the clipboard
    - Use track to show a progress bar while waiting for a response from the
      chatbot

--- chatgpt_commit_messages/cli.py
+++ chatgpt_commit_messages/cli.py
@@ -1,6 +1,12 @@
-import click
 import sys
 import json
+import typer
+from rich.progress import track
+from rich.padding import Padding
+from rich.markdown import Markdown
+from rich import print
+from time import sleep
+import pyperclip
 
 from revChatGPT.revChatGPT import Chatbot
 
@@ -19,34 +25,24 @@ def get_diff():
     return data
 
 def get_chatbot_query(emoji, body, choices, commit_type, commit_scope, diff):
+    return "Generate a commit message for the following diff:\n\n" + diff
     query = "I will provide you with a git diff. I want you to generate a single commit message, briefly summarizing that diff. The header has to follow convenctional commit style.\n"
-
     query += "\n"
-
     query += f"Your commit header should start with ${'an appropriate github emoji' if emoji else ''} a commit type, followed by the scope of the changes in parentheses, followed by a colon, and then a brief description of the changes.\n"
     if commit_type:
         query += f"The type is ${commit_type}.'\n"
-
     if commit_scope:
         query += f"This scope is ${commit_scope}.'\n"
-
-    # query += "Here is an example header:\n"
-    # query += ":sparkles: feat(button): add a new feature\n"
-    # query += ":bug: fix(router): fix a bug\n"
-    # query += ":recycle: refactor(reducer): refactor the reducer\n\n"
-
     if body:
         query += "Summarize the most important changes in the commit body.\nYou don't need to include every change.\nThe body should not be longer than 6 lines."
     else:
         query += "Don't append a body, use only the commit header.\n"
 
     query += "Do not answer with anything but the generated commit message, and output your response in code fences.\n"
-
     query += "this is the commit's diff:\n\n"
-    query += "```\n"
+    query += "\n"
     query += diff
-    query += "```\n"
-
+    query += "\n"
 
     query += "\n"
     if choices > 1:
@@ -60,43 +56,30 @@ def get_chatbot_response(chatbot, query):
     response_obj = chatbot.get_chat_response(query)
     return response_obj["message"]
 
-@click.command()
-@click.option('--emoji',
-             default=True,
-             help="Whether to add Github-Style Emoji",
-              type=bool,
-              show_default=True)
-@click.option('--body',
-             default=True,
-             help="Whether to add a commit body",
-              type=bool,
-              show_default=True)
-@click.option('--choices',
-             default=1,
-             help="The number of choices to return",
-              type=int,
-              show_default=True)
-@click.option('--commit-type',
-              default=None,
-              help="The commit type (fix, feat, refactor). If empty, it will be inferred",
-              type=str)
-@click.option('--commit-scope',
-              default=None,
-              help="The commit scope. If empty, it will be inferred",
-              type=str)
-@click.option('--dry-run', '-d',
-              is_flag=True,
-              help="Whether to print the query and exit")
-def main(emoji, body, choices, commit_type, commit_scope, dry_run):
+def main(
+    emoji: bool = typer.Option(True, help="Whether to add Github-Style Emoji"),
+    body: bool = typer.Option(True, help="Whether to add a commit body"),
+    choices: int = typer.Option(1, help="The number of choices to return"),
+    commit_type: str = typer.Option(None, help="The commit type. If empty, it will be inferred"),
+    commit_scope: str = typer.Option(None, help="The commit scope. If empty, it will be inferred"),
+    dry_run: bool = typer.Option(False, help="Whether to print the query and exit", is_flag=True),
+):
     config = get_config()
     diff = get_diff()
     query = get_chatbot_query(emoji, body, choices, commit_type, commit_scope, diff)
     if dry_run:
-        print(query)
+        query_output = "# Query\n\n" + query
+        print(Padding(Markdown(query_output), (2, 4)))
+        pyperclip.copy(query)
         return
     chatbot = get_chatbot(config)
     response = get_chatbot_response(chatbot, query)
+    total = 0
+    for value in track(range(100), description="Generating commit messages..."):
+        sleep(0.01)
+        total += 1
+
     print(response)
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
