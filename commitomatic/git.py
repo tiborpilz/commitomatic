128ede27d8f0069d36ae1127c4dcae5b096067ee
Tibor Pilz <tibor@pilz.berlin>
2022-12-10 17:43:01 +0100

    🔧(cli): add fzf integration
    - add `FzfPrompt` class to `cli.py`
    - add `get_diff_files` and `set_files` methods to `git.py` to allow
      filtering of diff by files

--- commitomatic/git.py
+++ commitomatic/git.py
@@ -9,17 +9,23 @@ from git import Repo
 
 
 class Repository(object):
-    def __init__(self, path: Path):
+    def __init__(self, path: Path, staged: bool = True):
         self.diff_target = None
         self.path = path
         # TODO: error handling
         self.repo = Repo(path)
         assert not self.repo.bare
         self.git = self.repo.git
+        self.files = None
+        self.flags = ["--staged"] if staged else []
 
     def get_diff(self, *flags, **kwargs):
         params = [f"--{key} {value}" for key, value in kwargs.items()]
-        return self.git.diff(self.diff_target, *flags, *params)
+        file_args = ["--", *self.files] if self.files is not None else []
+        return self.git.diff(self.diff_target, *self.flags, *flags, *params, *file_args)
 
     def get_diff_files(self):
         return self.get_diff("--name-only").split("\n")
+
+    def set_files(self, files):
+        self.files = files
